64bf63a435c3147ea8a14dadc3d66bebc4270d33
Ken Lai <ken1029@gmail.com>
2017-04-06 16:41:13 +0800

    Change MAC2 to MAC1 for G50 platform
    Signed-off-by: Ken Lai <ken1029@gmail.com>

--- include/configs/ast-g4-phy.h
+++ include/configs/ast-g4-phy.h
@@ -112,10 +112,10 @@
 * 3: Reserved
 *-------------------------------------------------------------------------------
 */
-#define CONFIG_MAC1_PHY_SETTING		2
+#define CONFIG_MAC1_PHY_SETTING		0
 #define CONFIG_MAC2_PHY_SETTING		0
-#define CONFIG_ASPEED_MAC_NUMBER  2
-#define CONFIG_ASPEED_MAC_CONFIG  2 
+#define CONFIG_ASPEED_MAC_NUMBER  1
+#define CONFIG_ASPEED_MAC_CONFIG  1 
 #define _PHY_SETTING_CONCAT(mac) CONFIG_MAC##mac##_PHY_SETTING
 #define _GET_MAC_PHY_SETTING(mac) _PHY_SETTING_CONCAT(mac)
 #define CONFIG_ASPEED_MAC_PHY_SETTING \
